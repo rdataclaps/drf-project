cec28f9e444cc2ba7a19bc4332ebf336bcd09524
rdataclaps <rahuldevpy@gmail.com>
2024-03-21 23:46:48 +0530

    created new model

--- students/models.py
+++ students/models.py
@@ -5,3 +5,8 @@ class Student(models.Model):
     name = models.CharField(max_length = 255, null=False)
     roll =  models.IntegerField()
     city = models.CharField(max_length =255)
+
+class Teacher(models.Model):
+    name = models.CharField(max_length = 255, null=False)
+    Subject = models.CharField(max_length = 255, null=False)
+    joining_date =  models.DateField()
