7eede8d98d47403e75465021e46f8e8fb0f157bd
rdataclaps <rahul.dataclaps@gmail.com>
2024-03-23 23:56:37 +0530

    url updated

--- students/urls.py
+++ students/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register(r'students-info', views.StudentViewSet)
+router.register(r'student', views.StudentViewSet)
 
 
 urlpatterns = [
