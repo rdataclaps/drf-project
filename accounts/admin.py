e2d518fab82700c9cfa078fe227f1054cc1814dd
rdataclaps <rahul.dataclaps@gmail.com>
2024-03-25 23:46:28 +0530

    Change user admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,4 +3,6 @@ from django.contrib import admin
 from accounts.models import User
 
 # Register your models here.
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'email']
